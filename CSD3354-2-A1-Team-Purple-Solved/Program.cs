d7bf4859f631a62bdcc708b04339e7793fdbfda3
visualstudio <visualstudio@122-PC25>
2019-02-28 17:31:55 -0500

    The Result is I am in Alst I am in Schvenig I am in Maeland I am in Helmholtz I am in Wessig I am in Uster I am in Badden I found Dear Astrildge in Badden **** FEELING HAPPY!!! ******  0 -- Alst *** --- 0  14 -- Schvenig *** --- 14  9 -- Maeland *** --- 9  28 -- Helmholtz *** --- 28  19 -- Wessig *** --- 19  28 -- Uster *** --- 28  11 -- Badden *** --- 11 Astrilde, I walked 218 vika to find you. Will you marry me? An Announcement was posted in "The Viking Village Newspaper", later that week:
    The Familes of Astridle Gu?mundsd?ttir and Hugi ?lafur
    are Happy to announce the Marriage of their Children.
    
    The Ceremony will take place on the third Sunnudagur of G?a
    at the Temple of Frigga in Alst.
    
    All are Welcome.
    Press any key to continue . . .

--- CSD3354-2-A1-Team-Purple-Solved/Program.cs
+++ CSD3354-2-A1-Team-Purple-Solved/Program.cs
@@ -1,12 +1,129 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace CSD3354_2_A1_Team_Purple_Solved
 {
-    class Program
+    class JournalEntry
     {
-        static void Main(string[] args)
+        public JournalEntry(string note, int dist)
         {
-            Console.WriteLine("Hello World!");
+            villageName = note; distanceTraveled = dist;
+            HowFarToGetBack = distanceTraveled;
         }
+        public int HowFarToGetBack = 0;
+        private string villageName;
+        private int distanceTraveled;
+        public int getDistanceWalked() { return distanceTraveled; }
+        public string getVillageName() { return villageName; }
+    }
+    class Hugi
+    {
+        private static JournalEntry je;
+        public static bool FoundAstrilde = false;
+        public static List<JournalEntry> HugiJournal = new List<JournalEntry>();
+        public static int CalculateDistanceWalked()
+        {
+            int DistanceWalked = 0;
+            foreach (var je in HugiJournal)
+            {
+                Console.WriteLine(" {0} -- {1} *** --- {2} ", je.getDistanceWalked(), je.getVillageName(), je.HowFarToGetBack);
+                DistanceWalked += je.getDistanceWalked() + je.HowFarToGetBack;
+            }
+            return DistanceWalked;
+        }
+    }
+    class CountrySide
+    {
+        static void Main()
+        {
+            CountrySide a = new CountrySide();
+            a.Run();
+            a.Announcement();
+        }
+        Village Maeland;
+        Village Helmholtz;
+        Village Alst;
+        Village Wessig;
+        Village Badden;
+        Village Uster;
+        Village Schvenig;
+        public void TraverseVillages(Village CurrentVillage)
+        {
+            if (Hugi.FoundAstrilde) return;
+            Hugi.HugiJournal.Add(new JournalEntry(CurrentVillage.VillageName, CurrentVillage.distanceFromPreviousVillage));
+            Console.WriteLine("I am in {0}", CurrentVillage.VillageName);
+            try
+            {
+                if (CurrentVillage.isAstrildgeHere)
+                {
+                    Console.WriteLine("I found Dear Astrildge in {0}", CurrentVillage.VillageName);
+                    Console.WriteLine("**** FEELING HAPPY!!! ******");
+                    Console.WriteLine("Astrilde, I walked {0} vika to find you. Will you marry me?", Hugi.CalculateDistanceWalked());
+                    Hugi.FoundAstrilde = true;
+                }
+                TraverseVillages(CurrentVillage.west);
+                TraverseVillages(CurrentVillage.east);
+            }
+            catch (Exception nre)
+            {
+            }
+        }
+        public void Run()
+        {
+            Alst = new Village("Alst", false);
+            Schvenig = new Village("Schvenig", false);
+            Wessig = new Village("Wessig", false);
+            Maeland = new Village("Maeland", false);
+            Helmholtz = new Village("Helmholtz", false);
+            Uster = new Village("Uster", false);
+            Badden = new Village("Badden", true);
+            Alst.VillageSetup(0, Schvenig, Wessig);
+            Schvenig.VillageSetup(14, Maeland, Helmholtz);
+            Wessig.VillageSetup(19, Uster, Badden);
+            Maeland.VillageSetup(9, null, null);
+            Helmholtz.VillageSetup(28, null, null);
+            Uster.VillageSetup(28, null, null);
+            Badden.VillageSetup(11, null, null);
+            this.TraverseVillages(Alst);
+        }
+        public void Announcement()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader("TeamPurple.txt"))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+    class Village
+    {
+        public Village(string _villageName, bool _isAHere)
+        {
+            isAstrildgeHere = _isAHere;
+            VillageName = _villageName;
+        }
+        public void VillageSetup(int _prevVillageDist, Village _westVillage, Village _eastVillage)
+        {
+            east = _eastVillage;
+            west = _westVillage;
+            distanceFromPreviousVillage = _prevVillageDist;
+        }
+        public Village west;
+        public Village east;
+        public string VillageName;
+        public int distanceFromPreviousVillage;
+        public bool isAstrildgeHere;
     }
 }
